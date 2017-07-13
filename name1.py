31390df57ed6290cf056d6cf8defc054da2cb402
ednamary <dshrestha26@gmail.com>
2017-07-12 21:16:45 -0700

    modified name1.py: added Concatenating code

--- name1.py
+++ name1.py
@@ -1,3 +1,10 @@
+#Changing Case in a String with Methods
 name = "Ada Lovelace"
 print(name.upper())
 print(name.loer())
+
+#Combining or Concatening Strings
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
